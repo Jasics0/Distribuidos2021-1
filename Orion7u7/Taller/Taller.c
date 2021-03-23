b6d62e1b10c3eaf0cbf451fcffc00e8fa693d5f0
orion7u7 <73809888+orion7u7@users.noreply.github.com>
2021-03-23 17:24:35 -0500

    xd

---
+++ Orion7u7/Taller/Taller.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+struct data
+{
+    int a,b;
+};
+
+FILE *archivo(){
+    FILE *fp=fopen("sample.txt","r");
+    if(fp=NULL){
+        printf("No se pudo leer el archivo");
+    }
+    return fp;
+    
+}
+pthread_t threads [100];
+
+int main(int argc,char **argv)
+{
+   struct data array[5];
+   int i;
+   int num=0;
+   num=atoi(argv[1]);
+   for (i=0;i<num;i++)
+   {
+       array[i].b=i+1;
+       pthread_create(&threads[i],NULL,(void *)&function,(void *)&array[i]); 
+   }
+   for (i=0;i<num;i++)
+   {
+     pthread_join(threads[i],NULL); 
+   }
+}
