f11696386dfdbc79e27eb1498953d27e9e54656f
Henry David Martinez <57374507+henrymartinez296@users.noreply.github.com>
2021-03-24 15:22:56 -0500

    Taller 1 Al fin completo uwur
    kiero k me des una salchipapa

--- Henrro/Taller1/taller1.c
+++ Henrro/Taller1/taller1.c
@@ -3,25 +3,31 @@
 #include <stdlib.h>
 // ./program aaaa     bbb     ccc
 //   argv[0] argv[1]  argv[2] argv[3]
+FILE *datos;
 pthread_t threads[100];
+double total = 0;
 
 struct data
 {
-    int a, b;
+    long a, b;
 };
 
-void sumarNumeros(int n)
-{
-
-}
-
-void function(void *data)
+void sumarNumeros(void *data)
 {
     struct data *t_data = (struct data *)data;
-    while (1)
+    long a = (*t_data).a;
+    long b = (*t_data).b;
+    long i = 0;
+    long dato = 0;
+    long suma = 0;
+    for (int i = a; i < b; i++)
     {
-        printf("%d\n", t_data->b);
+        fscanf(datos, "%ld", &dato);
+        suma = suma + dato;
+        printf("La suma es: %ld\n", suma);
     }
+    total += suma;
+    printf("El total es: %f\n", total);
 }
 
 // void traerNumeros(char *arg, int *a){
@@ -52,22 +58,29 @@ void function(void *data)
 
 int main(int argc, char **argv)
 {
-    FILE *archivo;
-    archivo = fopen(argv[2], "r");
+    datos = fopen(argv[2], "r");
     int numhilos = atoi(argv[1]);
-    long i = 0;
-    long dato = 0;
-    while (feof(archivo) == 0)
+    int numdatos = atoi(argv[3]);
+    int n = numdatos / numhilos;
+    struct data *numeros = (struct data *)calloc(n, sizeof(struct data));
+    //int numbers[201] = {};
+    //traerNumeros(argv[2], numbers);
+    for (int i = 0; i < numhilos; i++)
     {
-        i++;
-        fscanf(archivo, "%ld", &dato);
+        numeros[i].a = (i * n);
+        numeros[i].b = ((i + 1) * n);
+        if (i == numhilos - 1 && numdatos % numhilos != 0)
+        {
+            int x = numdatos - (n * numhilos);
+            numeros[i].b = numeros[i].b + x;
+        }
+        pthread_create(&threads[i], NULL, (void *)&sumarNumeros, (void *)&numeros[i]);
+        pthread_join(threads[i], NULL);
     }
-    int numdatos = i;
-    int n = numdatos/numhilos;
-    int numbers[201] = {};
-    //traerNumeros(argv[2], numbers); 
+    double prom = total / numdatos;
     printf("\nNumero de hilos: %d\n", numhilos);
     printf("\nNumero de datos: %d\n", numdatos);
     printf("\nNumero de datos por cada hilo: %d\n", n);
-
+    printf("\nEl promedio de la suma de los datos es: %f\n", prom);
+    fclose(datos);
 }
